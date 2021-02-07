0fb0dbf2db9ebef81b91423a321ce1679250709a
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-02-07 19:35:10 +0400

    Güncellendi

--- LinqProject(7thDayHomework)/Program.cs
+++ LinqProject(7thDayHomework)/Program.cs
@@ -23,11 +23,109 @@ namespace LinqProject_7thDayHomework_
                 new Product{ProductId=5, CategoryId=2, ProductName="Apple Telefon", QuantityPerUnit="4 GB Ram", UnitPrice=8000, UnitInStock=0},
 
             };
+            //Test(products);
 
+            //GetProducts(products);
+
+            //AnyTest(products);
+
+            //FindTest(products);
+
+            //FindAllTest(products);
+
+            //AscDescTest(products);
+
+            //DifferentSpelling(products);
+
+            //ClassikLinqTest(products);
+
+            //Join - birleştirmek, equal - eşit
+            var result = from p in products join c in categories
+                         on p.CategoryId equals c.CategoryId
+                         where p.UnitPrice>5000
+                         orderby p.UnitPrice
+                         select new ProductDto { ProductId = p.ProductId, CategoryName = c.CategoryName, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+
+            foreach (var productDto in result)
+            {
+                //{0} yerine productDto.ProductName,  {1} yerine productDto.CategoryName
+                Console.WriteLine("{0} ---> {1}",productDto.ProductName,productDto.CategoryName);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void ClassikLinqTest(List<Product> products)
+        {
+            var result = from p in products
+                         where p.UnitPrice > 6000
+                         orderby p.UnitPrice descending, p.ProductName ascending
+                         select new ProductDto { ProductId = p.ProductId, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void DifferentSpelling(List<Product> products)
+        {
+            //Bu da farklı bir yazım şekli...products tan  her p takma isimli - kısaca bunun gibi ==> var result = products.FindAll(p => p.ProductName);
+            var result = from p in products
+                         where p.UnitPrice > 6000
+                         orderby p.UnitPrice descending, p.ProductName ascending
+                         select p;
+            foreach (var product in result)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        private static void AscDescTest(List<Product> products)
+        {
+            //Single Line Query - Tek Satırlı Sorgu
+
+            //içindeki top olan ürünleri getirir...FindAll gibi aynı işlemi yapar
+            //UnitPrice a göre yüksekten aşağıya doğru sıralar 
+            //ThenBy ile de a dan z ye sıralar
+            var result = products.Where(p => p.ProductName.Contains("top")).OrderBy(p => p.UnitPrice).ThenBy(p => p.ProductName);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void FindAllTest(List<Product> products)
+        {
+            //şarta uyan bütün elemanları getirir
+            var result = products.FindAll(p => p.ProductName.Contains("top"));
+            Console.WriteLine(result);
+            //çıktı:  System.Collections.Generic.List`1[LinqProject_7thDayHomework_.Product]
+            //direk olarak liste dönüyor
+        }
+        //************************************************************************************
+
+        private static void FindTest(List<Product> products)
+        {
+            //aradığımız kritere uygun nesnenin kendisini veriyor
+            //ürün detayına gitmek için kullanılır
+            var result = products.Find(p => p.ProductId == 3);
+            Console.WriteLine(result);  // çıktı:  LinqProject_7thDayHomework_.Product
+            Console.WriteLine(result.ProductName);// çıktı:  Hp Laptop
+        }
+        //************************************************************************************
+
+        private static void AnyTest(List<Product> products)
+        {
+            var result = products.Any(p => p.ProductName == "Acer Laptop");
+            Console.WriteLine(result); //true
+        }
+        //************************************************************************************
+        private static void Test(List<Product> products)
+        {
             Console.WriteLine("Algoritmik------------------------");
             foreach (var product in products)
             {
-                if (product.UnitPrice>5000 && product.UnitInStock>3)
+                if (product.UnitPrice > 5000 && product.UnitInStock > 3)
                 {
                     Console.WriteLine(product.ProductName);
                 }
@@ -41,14 +139,9 @@ namespace LinqProject_7thDayHomework_
             {
                 Console.WriteLine(product.ProductName);
             }
-
-            GetProducts(products);
-
-
-            Console.ReadLine();
         }
 
-        //*********************************************************************
+        //************************************************************************************
 
         static List<Product> GetProducts(List<Product> products)
         {
@@ -71,6 +164,15 @@ namespace LinqProject_7thDayHomework_
         }
 
 }
+    class ProductDto //dto - data transformation object
+    {
+        public int ProductId { get; set; }
+        public string  CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+
+
 
     class Product
     {
